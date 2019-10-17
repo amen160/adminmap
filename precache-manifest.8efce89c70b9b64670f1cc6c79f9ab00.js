aa66602d351506d08fa699a5920567bf067531bf
Duy Phan <ducduymm0@gmail.com>
2019-10-17 17:10:36 +0700

    Updates

---
+++ precache-manifest.8efce89c70b9b64670f1cc6c79f9ab00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3ad142f9b096e0fd6a9f60c8d34123",
+    "url": "/adminmap/index.html"
+  },
+  {
+    "revision": "519fa8aac7924c142456",
+    "url": "/adminmap/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6c67bcc8160072c5a2f5",
+    "url": "/adminmap/static/js/2.56d3b8c4.chunk.js"
+  },
+  {
+    "revision": "519fa8aac7924c142456",
+    "url": "/adminmap/static/js/main.77d28f24.chunk.js"
+  },
+  {
+    "revision": "c7fb0066bf9d2bb07120",
+    "url": "/adminmap/static/js/runtime~main.f17c8e2c.js"
+  }
+]);
